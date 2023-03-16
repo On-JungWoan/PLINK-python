9885124fe1cfbf04f5231ee46a4778f5f95d7c22
on-jungwoan <slalfpdl16@naver.com>
2023-03-17 00:58:33 +0900

    qweqwe

--- main.py
+++ main.py
@@ -7,36 +7,47 @@ from regression import linear_regression, logistic_regression
 
 def get_args_parser():
     parser = argparse.ArgumentParser('Default Argument', add_help=False)
-
     # dataset parameters
     parser.add_argument('--path', type=str, default='')
-    parser.add_argument('--mode', default='logistic')
+    parser.add_argument('--mode', default='linear')
+    parser.add_argument('--file', default='bim')
 
     return parser
 
+def make_xy(args, df):
+
+    # pheno
+    train_test_df = merge_col(
+        df, join(args.path, f'dataset/{args.mode}_pheno.txt'), 'y'
+    )
+
+    return train_test_df.drop(['i'], axis=1)
+
 def main(args):
     bim, fam, bed = read_plink(join(args.path, 'dataset/data')) #bim, fam, bed
-    # fam = read_vcf(join(args.path, 'dataset/data.vcf'))
-    genotype_df = merge_col(
-        fam, join(args.path, 'dataset/sex_info.txt'), 'sex'
-    )  
+    fam = read_vcf(join(args.path, 'dataset/data.vcf'))
+
+    if args.file == 'fam':
+        # add sex_info
+        genotype_df = merge_col(
+            fam, join(args.path, 'dataset/sex_info.txt'), 'sex'
+        )
+        # add covar
+        genotype_df = merge_col(
+            genotype_df, join(args.path, f'dataset/covar.txt'), 'cov'
+        )        
+
+    train_test_df = make_xy(args, genotype_df)
+    # print(f"**bim file**\n{bim}\n")
+    # print(f"**fam file**\n{fam}")
 
     if args.mode == 'linear':
-        train_test_df = merge_col(
-            genotype_df, join(args.path, f'dataset/{args.mode}_pheno.txt'), 'y'
-        )
-        print(f"**bim file**\n{bim}\n")
-        print(f"**fam file**\n{fam}")
-        # print(linear_regression(train_test_df))
+        print(linear_regression(args, train_test_df))
     elif args.mode == 'logistic':
-        train_test_df = merge_col(
-            genotype_df, join(args.path, f'dataset/{args.mode}_pheno.txt'), 'y'
-        )
-        print(f"**bim file**\n{bim}\n")
-        print(f"**fam file**\n{fam}")        
-        # print(logistic_regression(train_test_df))
+        print(logistic_regression(args, train_test_df))
     else:
         sys.exit(0)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Arguments', parents=[get_args_parser()])
--- regression/__init__.py
+++ regression/__init__.py
@@ -1,16 +1,22 @@
 from statsmodels.formula.api import logit
 import statsmodels.api as sm
 import pandas as pd
+import sys
+
 #전처리함수
-def pre_process(data):
+def pre_process(args, data):
     NoIdData = data.drop(['fid','iid'], axis=1)#id가 담긴 데이터들 제거
-    NoIdData['y'] = NoIdData['y'].astype(int)
+    if args.mode == 'linear':
+        NoIdData['y'] = NoIdData['y'].astype(float)
+    elif args.mode == 'logistic':
+        NoIdData['y'] = NoIdData['y'].astype(float)
+    else:
+        sys.exit(0)
     NoIdData = NoIdData.replace({'y' : 1}, 0)
     NoIdData = NoIdData.replace({'y' : 2}, 1)
     NoIdData['father'] = NoIdData['father'].astype(int)
     NoIdData['mother'] = NoIdData['mother'].astype(int)
     NoIdData['gender'] = NoIdData['gender'].astype(int)
-    NoIdData['i'] = NoIdData['i'].astype(int)
     NoIdData['sex'] = NoIdData['sex'].astype(int)
     NoIdData['trait'] = NoIdData['trait'].astype(int)
     y_cols=['y']
@@ -18,15 +24,15 @@ def pre_process(data):
     print(NoIdData)
     return NoIdData, x_cols, y_cols
 
-def logistic_regression(data):
-    newdata ,x_cols, y_cols = pre_process(data)
+def logistic_regression(args, data):
+    newdata ,x_cols, y_cols = pre_process(args, data)
     model = logit(f"{y_cols[0]}~"+"+".join(x_cols), data=newdata)
     results = model.fit()
-    return results.summary
+    return results.summary()
         # 다변수함수에 뉴턴방법을 적용한 로지스틱
 
-def linear_regression(data):
-    newdata ,x_cols, y_cols = pre_process(data)
+def linear_regression(args, data):
+    newdata ,x_cols, y_cols = pre_process(args, data)
     model = sm.OLS.from_formula(f"{y_cols[0]}~"+"+".join(x_cols), data=newdata)
     results = model.fit()
-    return results.summary
+    return results.summary()
